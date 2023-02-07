d7befd9c9c0e69aa8eae028a819a1c6961057515
Thomas Fournier <thomas42600.f@gmail.com>
2023-02-07 21:38:32 +0100

    cleaning

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,24 +1,46 @@
+
 import React from "react";
 
+/**
+ * Props of the component
+ */
 type Props = {
     parent: (elem : string) => void;
     content: string[];
 }
 
+/**
+ * Compoment to display a list of elements
+ * @param parent A function to inform the parent component that an element is selected
+ * @param content The list of elements to display
+ */
 const List: React.FC<Props> = ({ parent, content}) => {
-
     return (
         <div className="container">
-            <div>
+            <section style={styles.list}>
                 {content.map((item: string) => (
-                    <div key={item} onClick={() => parent(item)}>
-                        <p>{item}</p>
+                    <div style={styles.tile} key={item} onClick={() => parent(item)}>
+                        <h5>{item}</h5>
                     </div>
                 ))}
-            </div>
+            </section>
         </div>
     );
-
 };
+
+// CSS-In-JS style attributes (to have a completely autonomous component)
+const styles = {
+    list: {
+        "display": "flex",
+        "flex-wrap": "wrap",
+        "alignItems": "center",
+    },
+    tile: {
+        "backgroundColor": "#e1e1ea",
+        "padding": "30px",
+        "margin": "30px",
+        "borderRadius": "10px",
+    }
+}
 
 export default List;
