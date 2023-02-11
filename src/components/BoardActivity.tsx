7ffb5594a71028d65f666488bd5121e4b34996d1
KratosLox <feunnec11@gmail.com>
2023-02-11 14:57:18 +0100

    Jeux : SearchBar

--- src/components/BoardActivity.tsx
+++ src/components/BoardActivity.tsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Button from '@mui/material/Button';
 
+
 import List from "./List";
 import { getActivity } from "../services/activity.service"
+import { valueToPercent } from "@mui/base";
 
 const BoardActivity: React.FC = () => {
   const [state, setState] = useState<boolean>(true);
   const [list, setList] = useState<any[]>([]);
   const [detail,setDetail] = useState<string>("")
+  const [seachTerm, setSearchTerm] = useState<string>("")
+
 
   useEffect(() => {
     getActivity().then(
@@ -29,14 +33,39 @@ const BoardActivity: React.FC = () => {
   const back = () => {
     setState(true)
     setDetail("")
+    setSearchTerm("")
   }
   
+  const handleSearch = (event: any) => {
+    let value = event.target.value
+    setSearchTerm(value)
+  }
+
+  console.log(seachTerm)
 
   return(
     <div className="container">
-      <div>
-        {state ? <List parent={parent} content={list.map((elem:any) => elem.nameActivity)}/> : <div></div>}
-        {detail ? <div>
+      { state ? 
+          <div className="searchBar">
+            <input type="text" 
+            placeholder="Rechercher"
+            onChange={handleSearch}
+            
+            />
+            <List parent={parent} content={list.map((elem:any) => elem.nameActivity).filter((val:any) => {
+                if(seachTerm == ""){
+                  return val
+                }else if(val.toLowerCase().includes(seachTerm.toLowerCase())){
+                  return val
+                }
+              })}
+            />
+
+            </div>
+        : <div></div>
+      }
+
+      {detail ? <div>
           <Button variant="contained" color="primary" onClick={back}>
             Retour
           </Button>
@@ -47,7 +76,7 @@ const BoardActivity: React.FC = () => {
           <p>Informations</p>
         </div> 
         : <div></div>}
-      </div>
+        
     </div>
   );
 }
