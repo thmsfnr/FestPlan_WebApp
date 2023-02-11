1f4a7ce487f8b6207a44ddde424933d71f51d020
Thomas Fournier <thomas42600.f@gmail.com>
2023-02-11 14:54:55 +0100

    add style affect zone

--- src/components/AffectZone.tsx
+++ src/components/AffectZone.tsx
@@ -6,10 +6,17 @@ import List from "./List";
 import DetailAffectZone from "./DetailAffectZone";
 import { getZone } from "../services/zone.service";
 
+/**
+ * Props of the component
+ */ 
 type Props = {
     parent: () => void
 }
 
+/**
+ * Component to affect an activity to a zone
+ * @param parent A function to return to the parent component 
+ */
 const AffectZone: React.FC<Props> = ({ parent }) => {
   const [state, setState] = useState<boolean>(true);
   const [list, setList] = useState<any[]>([]);
@@ -26,26 +33,52 @@ const AffectZone: React.FC<Props> = ({ parent }) => {
     );
   }, []);
 
+  /**
+   * Manage the click on a list element
+   * @param elem The name of the element clicked
+   */
   const change=(elem : string) => {
     setState(false)
     const save = list.filter((item:any) => item.nameZone === elem)
     setDetail(save[0])
   }
 
+  /**
+   * Manage the click on the back button
+   */
   const back= () => {
     setState(true)
   }
 
   return(
     <div className="container">
-      <div>
-        {state ? <div><List parent={change} content={list.map((elem:any) => elem.nameZone)}/><Button variant="contained" color="primary" onClick={parent}>
-            Retour
-          </Button></div> : <DetailAffectZone parent={back} content={detail}/>}
-        
-      </div>
+      <section>
+        {state ? 
+          /* Display the list of zones */
+          <article>
+            <div style={styles.back} className="jumbotron">
+              <Button variant="outlined" color="primary" onClick={parent}>Retour</Button>
+            </div>
+            <List parent={change} content={list.map((elem:any) => elem.nameZone)}/>
+          </article> 
+          : 
+          /* Display the detail of the zone */
+          <article>
+            <DetailAffectZone parent={back} content={detail}/>
+          </article>
+        }
+      </section>
     </div>
   );
+}
+
+// CSS-In-JS style attributes (to have a completely autonomous component)
+const styles = {
+  back: {
+      "margin": "30px",
+      "marginBottom": "30px",
+      "text-align": "center"
+  },
 }
 
 export default AffectZone;
