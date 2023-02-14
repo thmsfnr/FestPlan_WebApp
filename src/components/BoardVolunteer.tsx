c8a62d4841263613fab443aa38a2a59246379c68
Thomas Fournier <thomas42600.f@gmail.com>
2023-02-14 14:21:49 +0100

    update style home

--- src/components/BoardVolunteer.tsx
+++ src/components/BoardVolunteer.tsx
@@ -4,7 +4,14 @@ import Button from '@mui/material/Button';
 import List from "./List";
 import { getVolunteer } from "../services/volunteer.service"
 
-const BoardVolunteer: React.FC = () => {
+ /**
+     * Props of the component
+     */ 
+ type Props = {
+  backMenu: () => void
+}
+
+const BoardVolunteer: React.FC<Props> = ({ backMenu }) => {
   const [state, setState] = useState<boolean>(true);
   const [list, setList] = useState<any[]>([]);
   const [detail,setDetail] = useState<string>("")
@@ -35,6 +42,10 @@ const BoardVolunteer: React.FC = () => {
 
   return(
     <div className="container">
+      {/* Back button */}
+      <header style={styles.header} className="jumbotron">
+        <Button variant="outlined" color="primary" onClick={backMenu}>Retour</Button>
+      </header>
       <div>
         {state ? <List parent={parent} content={list.map((elem:any) => elem.name)}/> : <div></div>}
         {detail ? <div>
@@ -51,6 +62,15 @@ const BoardVolunteer: React.FC = () => {
       </div>
     </div>
   );
+}
+
+// CSS-In-JS style attributes (to have a completely autonomous component)
+const styles = {
+  header: {
+    "display": "flex",
+    "justifyContent": "center",
+    "margin": "30px",
+  },
 }
 
 export default BoardVolunteer;
