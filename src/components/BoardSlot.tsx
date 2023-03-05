9e8fd9ffb3c6fd9ecd5baaeee035d43465f9811a
KratosLox <feunnec11@gmail.com>
2023-03-05 18:25:34 +0100

    Lister Zone, lister Crenaux, gestionCrenaux_v1

--- src/components/BoardSlot.tsx
+++ src/components/BoardSlot.tsx
@@ -2,6 +2,97 @@
 import React, { useState, useEffect } from "react";
 import Button from '@mui/material/Button';
 
+import { getVolunteerAssignment } from "../services/volunteer_assignment.service";
+
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import LinearProgress from '@mui/material/LinearProgress';
+import {
+  ViewState,
+} from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  WeekView,
+  DayView,
+  MonthView,
+  Appointments,
+  Toolbar,
+  DateNavigator,
+  ViewSwitcher,
+  AppointmentForm,
+  AppointmentTooltip,
+  TodayButton,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+const PREFIX = 'Demo';
+
+const classes = {
+  toolbarRoot: `${PREFIX}-toolbarRoot`,
+  progress: `${PREFIX}-progress`,
+};
+
+const StyledDiv = styled('div')({
+  [`&.${classes.toolbarRoot}`]: {
+    position: 'relative',
+  },
+});
+
+const StyledLinearProgress = styled(LinearProgress)(() => ({
+  [`&.${classes.progress}`]: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
+    left: 0,
+  },
+}));
+
+const ToolbarWithLoading = (
+  ({ children, ...restProps }:any ) => (
+    <StyledDiv className={classes.toolbarRoot}>
+      <Toolbar.Root {...restProps}>
+        {children}
+      </Toolbar.Root>
+      <StyledLinearProgress className={classes.progress} />
+    </StyledDiv>
+  )
+);
+
+const usaTime = (date: any) => new Date(date).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+
+const mapAppointmentData = (appointment:any) => ({
+  id: appointment.id,
+  startDate: usaTime(appointment.start.dateTime),
+  endDate: usaTime(appointment.end.dateTime),
+  title: appointment.summary,
+});
+
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  return `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+};
+
+const initialState = {
+  data: [],
+  loading: false,
+  currentDate: getCurrentDate(),
+  currentViewName: 'Week',
+};
+
+const reducer = (state:any, action:any) => {
+  switch (action.type) {
+    case 'setLoading':
+      return { ...state, loading: action.payload };
+    case 'setData':
+      return { ...state, data: action.payload.map(mapAppointmentData) };
+    case 'setCurrentViewName':
+      return { ...state, currentViewName: action.payload };
+    case 'setCurrentDate':
+      return { ...state, currentDate: action.payload };
+    default:
+      return state;
+  }
+};
+
 /**
  * Props of the component
  */ 
@@ -15,6 +106,66 @@ import Button from '@mui/material/Button';
  * @returns 
  */
 const BoardVolunteer: React.FC<Props> = ({ backMenu }) => {
+  const [volunteers, setVolunteers] = useState<any[]>([]);
+  const [myEvents, setEvents] = React.useState<any[]>([]);
+
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const {
+    data, loading, currentViewName, currentDate,
+  } = state;
+  const setCurrentViewName = React.useCallback((nextViewName:any) => dispatch({
+    type: 'setCurrentViewName', payload: nextViewName,
+  }), [dispatch]);
+  const setData = React.useCallback((nextData:any) => dispatch({
+    type: 'setData', payload: nextData,
+  }), [dispatch]);
+  const setCurrentDate = React.useCallback((nextDate:any) => dispatch({
+    type: 'setCurrentDate', payload: nextDate,
+  }), [dispatch]);
+  const setLoading = React.useCallback((nextLoading:any) => dispatch({
+    type: 'setLoading', payload: nextLoading,
+  }), [dispatch]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+    // Volunteers assigned to the zone
+    getVolunteerAssignment(undefined).then(
+      (response) => {
+        convertData(response);
+        volunteers.forEach((item) => {
+          let found = false;
+          response.forEach((item2: { idVolunteer: any; }) => {
+            if (item.idVolunteer === item2.idVolunteer) {
+              found = true;
+            }
+          });
+          if (!found) {
+            setVolunteers([...volunteers, item]);
+          }
+        }
+        );
+      },
+      (error) => {
+        window.location.reload();
+      }
+    );
+  }
+  fetchData();
+}, [setData, currentViewName, currentDate]);
+
+  const convertData = (data : any) => {
+    let events: any[] = [];
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+      events.push({
+        startDate: element.Slot.startDate,
+        endDate: element.Slot.endDate,
+        title: element.Zone.nameZone + " - " + element.Volunteer.name + " " + element.Volunteer.surname,
+      });
+    }
+    setEvents(events);
+  }
 
   return(
     <div className="container">
@@ -22,6 +173,41 @@ const BoardVolunteer: React.FC<Props> = ({ backMenu }) => {
       <header style={styles.header} className="jumbotron">
         <Button variant="outlined" color="primary" onClick={backMenu}>Retour</Button>
       </header>
+      <Paper>
+        <Scheduler
+          data={myEvents}
+          height={750}
+          locale="fr-FR"
+        >
+          <ViewState
+            currentDate={currentDate}
+            currentViewName={currentViewName}
+            onCurrentViewNameChange={setCurrentViewName}
+            onCurrentDateChange={setCurrentDate}
+          />
+          <DayView
+            startDayHour={7.5}
+            endDayHour={17.5}
+          />
+          <WeekView
+            startDayHour={7.5}
+            endDayHour={17.5}
+          />
+          <MonthView
+          />
+          <Appointments />
+          <Toolbar
+            {...loading ? { rootComponent: ToolbarWithLoading } : null}
+          />
+          <DateNavigator />
+          <TodayButton />
+          <ViewSwitcher />
+          <AppointmentTooltip
+            showCloseButton
+          />
+          <AppointmentForm />
+        </Scheduler>
+      </Paper>
     </div>
   );
 }
--- src/components/DetailAffectVolunteer.tsx
+++ src/components/DetailAffectVolunteer.tsx
@@ -10,14 +10,110 @@ import Paper from '@mui/material/Paper';
 import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
-  WeekView,
-  Appointments,
-  Toolbar,
-  ViewSwitcher,
   MonthView,
+  WeekView,
   DayView,
+  Appointments,
+  AppointmentForm,
+  AppointmentTooltip,
+  EditRecurrenceMenu,
+  AllDayPanel,
+  ConfirmationDialog,
+  Toolbar,
+  DateNavigator,
+  ViewSwitcher,
+  TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
+import { styled } from '@mui/material/styles';
+import LinearProgress from '@mui/material/LinearProgress';
 
+
+const PREFIX = 'Demo';
+
+const classes = {
+  toolbarRoot: `${PREFIX}-toolbarRoot`,
+  progress: `${PREFIX}-progress`,
+};
+
+const StyledDiv = styled('div')({
+  [`&.${classes.toolbarRoot}`]: {
+    position: 'relative',
+  },
+});
+
+const StyledLinearProgress = styled(LinearProgress)(() => ({
+  [`&.${classes.progress}`]: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
+    left: 0,
+  },
+}));
+
+const PUBLIC_KEY = 'AIzaSyBnNAISIUKe6xdhq1_rjor2rxoI3UlMY7k';
+const CALENDAR_ID = 'f7jnetm22dsjc3npc2lu3buvu4@group.calendar.google.com';
+
+const getData = (setData: any, setLoading: any) => {
+  const dataUrl = ['https://www.googleapis.com/calendar/v3/calendars/', CALENDAR_ID, '/events?key=', PUBLIC_KEY].join('');
+  setLoading(true);
+
+  return fetch(dataUrl)
+    .then(response => response.json())
+    .then((data) => {
+      setTimeout(() => {
+        setData(data.items);
+        setLoading(false);
+        console.log(data.items);
+      }, 600);
+    });
+};
+
+const ToolbarWithLoading = (
+  ({ children, ...restProps }:any ) => (
+    <StyledDiv className={classes.toolbarRoot}>
+      <Toolbar.Root {...restProps}>
+        {children}
+      </Toolbar.Root>
+      <StyledLinearProgress className={classes.progress} />
+    </StyledDiv>
+  )
+);
+
+const usaTime = (date: any) => new Date(date).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+
+const mapAppointmentData = (appointment:any) => ({
+  id: appointment.id,
+  startDate: usaTime(appointment.start.dateTime),
+  endDate: usaTime(appointment.end.dateTime),
+  title: appointment.summary,
+});
+
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  return `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+};
+
+const initialState = {
+  data: [],
+  loading: false,
+  currentDate: getCurrentDate(),
+  currentViewName: 'Week',
+};
+
+const reducer = (state:any, action:any) => {
+  switch (action.type) {
+    case 'setLoading':
+      return { ...state, loading: action.payload };
+    case 'setData':
+      return { ...state, data: action.payload.map(mapAppointmentData) };
+    case 'setCurrentViewName':
+      return { ...state, currentViewName: action.payload };
+    case 'setCurrentDate':
+      return { ...state, currentDate: action.payload };
+    default:
+      return state;
+  }
+};
 const currentDate = '2023-02-14';
 const schedulerData = [
   { startDate: '2023-02-14T09:45', endDate: '2023-02-14T11:00', title: 'Meeting' },
@@ -43,6 +139,23 @@ const DetailAffectVolunteer: React.FC<Props> = ({ parent, content }) => {
   const [volunteers, setVolunteers] = useState<any[]>([]);
   const [selected, setSelected] = useState<number>();
   const [myEvents, setEvents] = React.useState<any[]>([]);
+
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const {
+    data, loading, currentViewName, currentDate,
+  } = state;
+  const setCurrentViewName = React.useCallback((nextViewName:any) => dispatch({
+    type: 'setCurrentViewName', payload: nextViewName,
+  }), [dispatch]);
+  const setData = React.useCallback((nextData:any) => dispatch({
+    type: 'setData', payload: nextData,
+  }), [dispatch]);
+  const setCurrentDate = React.useCallback((nextDate:any) => dispatch({
+    type: 'setCurrentDate', payload: nextDate,
+  }), [dispatch]);
+  const setLoading = React.useCallback((nextLoading:any) => dispatch({
+    type: 'setLoading', payload: nextLoading,
+  }), [dispatch]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -79,7 +192,8 @@ const DetailAffectVolunteer: React.FC<Props> = ({ parent, content }) => {
     );
     }
     fetchData();
-  }, []);
+    getData(setData, setLoading);
+  }, [setData, currentViewName, currentDate]);
 
 
   /**
@@ -140,7 +254,6 @@ const DetailAffectVolunteer: React.FC<Props> = ({ parent, content }) => {
   }
 
   const convertData = (data : any) => {
-    console.log(data);
     let events: any[] = [];
     for (let i = 0; i < data.length; i++) {
       const element = data[i];
@@ -154,6 +267,7 @@ const DetailAffectVolunteer: React.FC<Props> = ({ parent, content }) => {
     console.log(events[0].startDate);
     setEvents(events);
   }
+  
 
 
   return(
@@ -195,20 +309,42 @@ const DetailAffectVolunteer: React.FC<Props> = ({ parent, content }) => {
       </section>
       <Paper>
         <Scheduler
-          data={myEvents}
+          data={schedulerData}
+          height={660}
         >
           <ViewState
             currentDate={currentDate}
-            
+            currentViewName={currentViewName}
+            onCurrentViewNameChange={setCurrentViewName}
+            onCurrentDateChange={setCurrentDate}
           />
           <DayView
-            startDayHour={9}
-            endDayHour={17}
+            startDayHour={7.5}
+            endDayHour={17.5}
+          />
+          <WeekView
+            startDayHour={7.5}
+            endDayHour={17.5}
+          />
+          <MonthView
           />
           <Appointments />
+          <Toolbar
+            {...loading ? { rootComponent: ToolbarWithLoading } : null}
+          />
+          <DateNavigator />
+          <TodayButton />
+          <ViewSwitcher />
+          <AppointmentTooltip
+            showOpenButton
+            showCloseButton
+            showDeleteButton
+          />
+          <AppointmentForm />
         </Scheduler>
       </Paper>
     </div>
+
   );
 }
 
